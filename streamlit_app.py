6e41e40b0d1312aee29eaf379ad567fe449c0439
sungchu <songzhu1030@gmail.com>
2025-08-22 16:41:29 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,7 @@ with st.echo():
 
         ## ========== Selenium 設定 ==========
         options = Options()
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--headless")
