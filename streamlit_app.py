50aaa33d1484f26944f38f089ea0ba47591a07b2
sungchu <songzhu1030@gmail.com>
2025-08-22 16:51:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 from collections import defaultdict
 import os
 from zhconv import convert
